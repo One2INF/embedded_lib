55510287d0c0135ebd624f422a7d8e15f48504ae
One2INF <goodbob256@gmail.com>
2022-09-06 02:06:09 +0000

    tinyqueue：修改参数名

--- tinyqueue.h
+++ tinyqueue.h
@@ -31,7 +31,7 @@ bool QUEUE_PushBack(QUEUE queue, void *data);
 bool QUEUE_PopFront(QUEUE queue);
 bool QUEUE_PopBack(QUEUE queue);
 
-bool QUEUE_Create(QUEUE queue, void *buff, size_t queue_size, size_t element_size);
+bool QUEUE_Create(QUEUE queue, void *buff, size_t capacity, size_t element_size);
 bool QUEUE_Destroy(QUEUE queue);
 bool QUEUE_Traverse(QUEUE queue, void(*func)(void*));
 bool QUEUE_Clear(QUEUE queue);
